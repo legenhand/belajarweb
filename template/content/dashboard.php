239a101ae9155941640f78722633547c760c659e
legenhand <firmansyah720.fs@gmail.com>
2019-09-18 16:58:03 +0700

    removed templates website

--- template/content/dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(!defined('INDEX')) die("");
-?>
-
-<H1>Selamat Datang di Aplikasi Manajemen pegawai</H1>
-<h3>Anda login sebagai Administrator<H3>
--- webroyal/content/dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(!defined('INDEX')) die("");
-?>
-
-<H1>Selamat Datang <?= $_SESSION['nama_lengkap'] ?> di Aplikasi Manajemen kiriman</H1>
-<h3>Anda login sebagai Administrator<H3>
